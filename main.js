d8d46a5ee9b9f96c9b8379ffef0110dad26f35f7
Robin de Mourat <robin.demourat@gmail.com>
2017-10-07 21:24:56 +0200

    add port listening

--- main.js
+++ main.js
@@ -29,4 +29,8 @@ app.get('/', function (req, res) {
     }
 })
 
-app.listen(3000)
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
