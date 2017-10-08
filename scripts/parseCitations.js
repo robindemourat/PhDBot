cb59a4ebb8b279e3149b9f905e68d77810844d5c
Robin de Mourat <robin.demourat@gmail.com>
2017-10-08 18:48:39 +0200

    improve typography

--- scripts/parseCitations.js
+++ scripts/parseCitations.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const dsv = require('d3-dsv');
 const markdown = require('markdown').markdown;
+const typo = require('typographic-base');
 
 module.exports = function(quotesPath, referencesPath, callback) {
   fs.readFile(referencesPath, 'utf8', (err1, str1) => {
@@ -8,7 +9,7 @@ module.exports = function(quotesPath, referencesPath, callback) {
     fs.readFile(quotesPath, 'utf8', (err2, str2) => {
       const quotes = dsv.csvParse(str2).map(entry => {
         return Object.assign(entry, {
-          contenu_html: markdown.toHTML('> ' + entry.contenu_md),
+          contenu_html: markdown.toHTML('> ' + typo(entry.contenu_md, {locale: 'fr'})),
           bibRef : references.find(ref => ref.id === entry.ref_id)
         });
       });
