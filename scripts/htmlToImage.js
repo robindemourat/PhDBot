ad5e38eb10b5396905e8d30d16263e1cde35362d
Robin de Mourat <robin.demourat@gmail.com>
2017-10-08 16:14:44 +0200

    improve cron

--- scripts/htmlToImage.js
+++ scripts/htmlToImage.js
@@ -2,7 +2,16 @@ var webshot = require('webshot');
 
 module.exports = function(html, path, callback) {
   webshot(html, path, {
-    siteType:'html'
+    siteType:'html',
+    windowSize: {
+      width: 900,
+      // height: 450 // twitter normal optimal height
+      height: 10 // setting a mini height to have well formed images
+    },
+    shotSize: { 
+      width: 'all',
+      height: 'all' 
+    }
   }, (err) => {
     callback(err);
   });
